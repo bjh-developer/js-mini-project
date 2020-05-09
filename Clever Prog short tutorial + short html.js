7551677c9629416c3d94e95a61265ced8d143dc2
bjh-developer <52413727+bjh-developer@users.noreply.github.com>
2020-05-09 20:03:30 +0800

    First commit

---
+++ Clever Prog short tutorial + short html.js
@@ -0,0 +1,27 @@
+//home.js is a tutorial by Clever Programmer (YT account), built in Visual Studio Code with Javascript, displayed in Chrome dev tool
+
+console.log('hello');
+alert("hello this is bjh");
+
+
+//variables
+var b = 'smoothie';
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+
+var age = prompt('what is your age?');
+var name = prompt('what is your name?')
+
+document.getElementById('someText').innerHTML = 'You are ' + age + '. Your name is ' + name + ". YAY, it works lol";
+
+
+//numbers in js
+var num1 = 5.7;
+console.log(5 * 10);
+num1 += 1;
+console.log(num1)
+num1 --;
+console.log(num1)
